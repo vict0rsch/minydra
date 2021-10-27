00c7b900738bfb7d355777410855a6c6ccb55c41
vict0rsch <vsch@pm.me>
2021-10-27 04:56:41 -0400

    add quicker `resolved_args` >>> from minydra import resolved_args >>> args = resolved_args()

--- minydra/__init__.py
+++ minydra/__init__.py
@@ -25,3 +25,19 @@ def parse_args(
         return wrapper
 
     return decorator
+
+
+def resolved_args(
+    verbose=0,
+    allow_overwrites=True,
+    warn_overwrites=True,
+    parse_env=True,
+    warn_env=True,
+):
+    return Parser(
+        verbose=verbose,
+        allow_overwrites=allow_overwrites,
+        warn_overwrites=warn_overwrites,
+        parse_env=parse_env,
+        warn_env=warn_env,
+    ).args.resolve()
