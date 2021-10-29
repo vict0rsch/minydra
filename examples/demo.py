8a4f6b5b8c84dc3ecaf373568c62a199967e91c6
vict0rsch <vsch@pm.me>
2021-10-29 18:16:38 -0400

    update demo

--- examples/demo.py
+++ examples/demo.py
@@ -1,4 +1,5 @@
 from minydra import MinyDict, resolved_args
+from pathlib import Path
 
 if __name__ == "__main__":
     # parse arbitrary args in 1 line
@@ -8,8 +9,14 @@ if __name__ == "__main__":
     if args.default:
         args = MinyDict.from_json(args.default).update(args)
 
+    # protect args in the rest of the code execution
+    args.freeze()
+
     # print the args in a nice orderly fashion
     args.pretty_print()
 
     # access args with dot/attribute access
     print(f'Using project "{args.log.project}" in {args.log.outdir}')
+
+    # save configuration
+    args.to_json(Path(args.log.outdir) / f"{args.log.project}.json")
